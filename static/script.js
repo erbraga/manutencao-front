4f804c65d9c017c91a5a35c6c3fdf77df2a75fd6
erbraga <erbraga@uol.com.br>
2025-12-16 01:01:36 -0300

    Transferência dos valores do formulário para a tabela

--- static/script.js
+++ static/script.js
@@ -1,6 +1,45 @@
-function teste(){
-    let a = 5;
-    let b = 8;
-    let c = a + b;
-    alert("Hello World");
+function Formulario (){
+    this.item = document.getElementById("item").value;
+    this.especificacaoKm = document.getElementById("especificacao-km").value;
+    this.especificacaoPrazo = document.getElementById("especificacao-prazo").value;
+    this.ultimaTrocaKm = document.getElementById("ultima-troca-km").value;
+    this.ultimaTrocaData = document.getElementById("ultima-troca-data").value;
 }
+
+
+class Tabela{
+
+    constructor(valores){
+        this.item = valores.item
+        this.especificacaoKm =  valores.especificacaoKm; 
+        this.especificacaoPrazo = valores.especificacaoPrazo;
+        this.ultimaTrocaKm =  valores.ultimaTrocaKm;
+        this.ultimaTrocaData =  valores.ultimaTrocaData;
+    }
+
+    atualizarItem(linha, classe, conteudo){
+        let cel = linha.insertCell();
+        cel.innerHTML = conteudo;
+        cel.classList.add(classe);
+    }
+
+    atualizar(){
+        //let tabela = document.getElementById('tabela');
+        let linha = document.getElementById('tabela').insertRow();
+        this.atualizarItem(linha, 'item', this.item);
+        this.atualizarItem(linha, 'km', this.especificacaoKm);
+        this.atualizarItem(linha, 'prazo', this.especificacaoPrazo);
+        this.atualizarItem(linha, 'km', this.ultimaTrocaKm);
+        this.atualizarItem(linha, 'data', this.ultimaTrocaData);
+        this.atualizarItem(linha, 'km', ' ');
+        this.atualizarItem(linha, 'data', ' ');
+        this.atualizarItem(linha, 'acoes', '<button class = "icone i-atualizar"></button><button class = "icone i-deletar"></button>');
+    } 
+}
+
+const lerFormulario = () => {
+    
+    let formulario = new Formulario();
+    let tabela = new Tabela(formulario)    
+    tabela.atualizar()
+};
